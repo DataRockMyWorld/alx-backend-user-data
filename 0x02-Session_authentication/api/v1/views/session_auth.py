3973b1447969ae45bf5ff5d1ab2df3a0a333ab3e
Jewel <jewelbansah@rocketmail.com>
2024-06-06 20:57:59 +0000

    fix tasks

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
+""" Module of Users views
 """
-Basic authentication
-"""
+import os
+from api.v1.views import app_views
+from models.user import User
+from flask import jsonify, request
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def session_auth() -> str:
-    """
-    POST /api/v1/auth_session/login
-    Login
+def session_auth():
+    """_summary_
     """
     email = request.form.get('email')
     password = request.form.get('password')
-
     if email is None or email == '':
         return jsonify({"error": "email missing"}), 400
-    if password is None or password = '':
+    if password is None or password == '':
         return jsonify({"error": "password missing"}), 400
-    users = User.search({'email': email})
+    users = User.search({"email": email})
     if not users or users == []:
         return jsonify({"error": "no user found for this email"}), 404
     for user in users:
@@ -25,7 +25,7 @@ def session_auth() -> str:
             from api.v1.app import auth
             session_id = auth.create_session(user.id)
             resp = jsonify(user.to_json())
-            session_name = os.getenv("SESSION_NAME")
+            session_name = os.getenv('SESSION_NAME')
             resp.set_cookie(session_name, session_id)
             return resp
     return jsonify({"error": "wrong password"}), 401
